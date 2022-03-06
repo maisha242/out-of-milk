9deca6868dc6e20e25b37f7640de88913a7717e2
maisha iqbal <maisha242@gmail.com>
2022-03-05 20:01:13 -0500

    readding the tables

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roomies;
+DROP TABLE IF EXISTS kitchen;
+DROP TABLE IF EXISTS items;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    creation date,
+    email VARCHAR(255)
+);
+
+CREATE TABLE roomies
+(
+    roomID SERIAL PRIMARY KEY,
+    roommates int REFERENCES users(id)
+);
+
+CREATE TABLE kitchen(
+    id SERIAL PRIMARY KEY,
+    items int REFERENCES items(id)
+);
+
+CREATE TABLE items(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    quantity int NOT NULL,
+    buydate date
+);
