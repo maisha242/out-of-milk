4b91f21dada3a6cbe5cd5928c9fc5f4118042cd0
soweeby <rgnunna@gmail.com>
2022-03-06 11:12:26 -0500

    ]

--- out-of-milk/src/main/java/com/api/outofmilk/controller/KitchenController.java
+++ out-of-milk/src/main/java/com/api/outofmilk/controller/KitchenController.java
@@ -3,11 +3,12 @@ package com.api.outofmilk.controller;
 import com.api.outofmilk.model.Item;
 import com.api.outofmilk.model.Kitchen;
 import com.api.outofmilk.persistence.KitchenDAO;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 import java.util.logging.Logger;
 
 @RestController
@@ -24,6 +25,7 @@ public class KitchenController {
     @GetMapping("/{kid}")
     public ResponseEntity<Item[]> getKitchenItems(@PathVariable int kid) {
         LOG.info("Getting items!");
+
         System.out.println(kitchenDao.getItems(kid));
         return new ResponseEntity<Item[]>(kitchenDao.getItems(kid).toArray(new Item[0]), HttpStatus.OK);
     }
